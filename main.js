b9ec77f9c7fe4b01728f7083d3af74f680a2bd27
vue后台管理系统 <2953282238@qq.com>
2022-07-01 18:31:19 +0800

    7.11

--- main.js
+++ main.js
@@ -2,6 +2,37 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+
+// 分别暴露是为了按需倒入
+import { $http } from '@escook/request-miniprogram'
+// 挂载到全局对象上
+uni.$http = $http
+// 设置根路径
+uni.$http.baseUrl = 'https://www.uinav.com'
+
+// 请求失败处理
+uni.$showMsg = function(title="数据加载失败!",duration=1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:"none"
+	})
+}
+
+// 请求拦截器
+$http.beforeRequest = function(options){
+	// options 请求相关配置
+	// 微信上的api 都可以通过uni来进行调用
+	// loading效果
+	uni.showLoading({
+		title:'数据加载中...',
+	})
+}
+// 响应拦截器
+$http.afterRequest = function(){
+	uni.hideLoading()
+}
+
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,45 @@
 <template>
 	<view>
-		home
+		<!-- 轮播图 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="(item) in swiperList" :key="item.goods_id">
+				<navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id">
+					<image :src="item.image_src"></image>
+				</navigator>
+			</swiper-item>
+
+		</swiper>
+
+		<!-- 分类导航区域 -->
+		<view class="nav-list">
+			<view class="nav-item" v-for="item in navList" :key="item.name" @click="navClickHandler(item)">
+				<image :src="item.image_src" class="nav-img"></image>
+			</view>
+		</view>
+
+		<!-- 楼层区域 -->
+		<view class="floor-list">
+			<!-- 每一个楼层的item项 -->
+			<view class="floor-item" v-for="item in floorList" :key="item.floor_title.name" >
+				<!-- 楼层标题 -->
+				<image :src="item.floor_title.image_src" class="floor-title"></image>
+				
+				<view class="floor-img-box">
+					<!-- 左侧大图 -->
+					<view class="left-img-box">
+						<image :src="item.product_list[0].image_src" class="left-big-img"></image>
+					</view>
+					<!-- 右侧图 -->
+					<view class="right-img-box">
+						<view class="right-img-Box" v-for="itemx in item.product_list.slice(1)">
+							<image :src="itemx.image_src" class="right-img"></image>
+						</view>
+					</view>
+				</view>	
+			</view>
+			
+			
+		</view>
 	</view>
 </template>
 
@@ -8,12 +47,115 @@
 	export default {
 		data() {
 			return {
-				
-			};
-		}
+				// 轮博图
+				swiperList: [],
+				// 分类列表
+				navList: [],
+				// 楼层的数据
+				floorList: [],
+			}
+		},
+		onLoad() {
+			//获取轮播图
+			this.getSwiperlist();
+			this.getNavList();
+			this.getFloorList()
+		},
+		methods: {
+			async getSwiperlist() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				// 请求失败 调用封装好的失败处理函数
+				if (res.meta.status !== 200) return uni.$showMsg();
+				this.swiperList = res.message
+				// uni.$showMsg('请求成功')
+			},
+			async getNavList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/catitems')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.navList = res.message;
+				console.log(res)
+			},
+			async getFloorList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/floordata')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				console.log(res.message)
+				this.floorList = res.message
+			},
+
+
+			// 切换分类
+			navClickHandler(item) {
+				console.log(item)
+				if (item.name === '分类') {
+					uni.switchTab({
+						url: '/pages/cate/cate'
+					})
+				}
+			}
+		},
+
+
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%
+		}
+	}
+
+	.nav-list {
+		display: flex;
+		justify-content: space-around;
+		margin: 15px 0;
+
+		.nav-img {
+			width: 128rpx;
+			height: 140rpx;
+		}
+	}
+	
+	.floor-title{
+		width: 100%;
+		height: 60rpx;
+	}
+	
+	.floor-img-box{
+		display: flex;
+		justify-content: space-around;
+		// align-items: center;
+		.left-big-img{
+			width: 100%;
+		}
+		.left-img-box{
+			width: 36%;
+			flex-shrink: 0;
+		}
+		.right-img-box{
+			width: 62%;
+			flex-shrink: 0;
+			display: flex;
+			flex-wrap: wrap;
+			.right-img-Box{
+				width: 48%;
+				display: flex;
+				flex-wrap: wrap;
+			}
+			.right-img{
+				width: 97%;
+				height: 97%;
+			}
+		}
+	}
 </style>
